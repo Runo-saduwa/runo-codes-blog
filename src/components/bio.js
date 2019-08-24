d79a86e5b652eb7f773d5ede8889dc51696ca1bc
ogheneruno <orunosaduwa@gmail.com>
2019-08-24 23:54:45 +0100

    first commit

--- src/components/bio.js
+++ src/components/bio.js
@@ -54,12 +54,14 @@ const Bio = () => {
         }}
       />
       <p>
-        Written by <strong>{author}</strong> who lives and works in San
-        Francisco building useful things.
-        {` `}
-        <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
-        </a>
+        Personal blog by <strong> <a href={`https://twitter.com/${social.twitter}`}>
+        {author}
+        </a></strong> 
+        <br/>
+        I write about interesting topics in software development, Enjoy!
+      <br/>
+       
+
       </p>
     </div>
   )
